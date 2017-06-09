4d9a8e821bc7b5cbabfada4af4e7149f39205f16
yashg7cr <yashwin.yashwin3@gmail.com>
2017-06-09 16:53:17 +0530

    Create startinstall.ps1

--- custom/MS SQL Express/startinstall.ps1
+++ custom/MS SQL Express/startinstall.ps1
@@ -1,3 +1,61 @@
+[CmdletBinding()]
+param(
+)
+
+###################################################################################################
+
+#
+# PowerShell configurations
+#
+
+# NOTE: Because the $ErrorActionPreference is "Stop", this script will stop on first failure.
+#       This is necessary to ensure we capture errors inside the try-catch-finally block.
+$ErrorActionPreference = "Stop"
+
+# Ensure we set the working directory to that of the script.
+pushd $PSScriptRoot
+
+###################################################################################################
+
+#
+# Functions used in this script.
+#
+
+function Handle-LastError
+{
+    $message = $error[0].Exception.Message
+    if ($message)
+    {
+        Write-Host -Object "ERROR: $message" -ForegroundColor Red
+    }
+    
+    # IMPORTANT NOTE: Throwing a terminating error (using $ErrorActionPreference = "Stop") still
+    # returns exit code zero from the PowerShell script when using -File. The workaround is to
+    # NOT use -File when calling this script and leverage the try-catch-finally block and return
+    # a non-zero exit code from the catch block.
+    exit -1
+}
+
+###################################################################################################
+
+#
+# Handle all errors in this script.
+#
+
+trap
+{
+    # NOTE: This trap will handle all errors. There should be no need to use a catch below in this
+    #       script, unless you want to ignore a specific error.
+    Handle-LastError
+}
+
+###################################################################################################
+
+#
+# Main execution block.
+
+try
+{
 # Before running the script, set the execution policy
 Set-ExecutionPolicy RemoteSigned
 #
@@ -114,12 +172,7 @@ Start-Process -FilePath "$setupFolder\SSDTSetup.exe" -ArgumentList '/INSTALLALL=
 Add-PSSnapin SqlServerCmdletSnapin* -ErrorAction SilentlyContinue   
 Import-Module SQLPS -WarningAction SilentlyContinue  
 
-$AttachCmd = @"  
-USE [master]  CREATE DATABASE [AdventureWorks2012] ON (FILENAME ='$setupFolder\..\datasets\AdventureWorks2012_Data.mdf') for ATTACH  
-"@  
-Invoke-Sqlcmd $attachCmd -QueryTimeout 3600 -ServerInstance $env:computername\CB2016SQLSERVER 
-If($?)  
-{  
+
 	Write-Host 'Attached database sucessfully!'  
 }  
 else  
@@ -129,3 +182,10 @@ else
 
 
 Write-Host 'Installation completed.'
+    
+Write-Output 'Done!'
+}
+finally
+{
+    popd
+}
